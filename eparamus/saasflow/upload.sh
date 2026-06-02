db17a7d583185554ed6fcb08e9c99e1c5d57eee4
Ben Thomas <ben@benjit.com>
2026-06-01 21:48:01 -0400

    checkpoint before fetching

---
+++ eparamus/saasflow/upload.sh
@@ -0,0 +1,3 @@
+rsync -r theme/ root@67.205.186.94:/var/www/html/wp-content/themes/eparamus
+rsync -r content/ root@67.205.186.94:/var/www/html/content
+rsync -r Makefile root@67.205.186.94:/var/www/html/Makefile
